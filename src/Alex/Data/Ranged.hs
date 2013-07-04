8c43d29c5e466ab4ccfe62e41470cb613c6b9bcc
Jonas-Hugo <jonas.hugo.86@gmail.com>
2013-07-04 21:14:03 +0200

    Added Alex for BuildDFA to work

---
+++ src/Alex/Data/Ranged.hs
@@ -0,0 +1,9 @@
+module Alex.Data.Ranged (
+   module Alex.Data.Ranged.Boundaries,
+   module Alex.Data.Ranged.Ranges,
+   module Alex.Data.Ranged.RangedSet
+) where
+
+import Alex.Data.Ranged.Boundaries
+import Alex.Data.Ranged.Ranges
+import Alex.Data.Ranged.RangedSet
