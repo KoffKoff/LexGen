7514b006667194d308a1d5b30ccbb77f20eead5f
KoffKoff <koffkoffh@gmail.com>
2013-10-02 18:04:04 +0200

    automated the benchamrking some for

--- src/Test/Bench.hs
+++ src/Test/Bench.hs
@@ -7,7 +7,7 @@ import System.IO
 import Criterion.Main
 import Criterion.Config
 import Test.Java as J
-import Test.Lexjava
+import Test.Lexjava as A
 import Control.DeepSeq
 import Data.FingerTree as F
 import Data.Map as M (foldrWithKey',insert,empty)
@@ -16,6 +16,9 @@ import Data.Sequence as S hiding (zip)
 import Data.Monoid
 import Data.Char
 import Data.Array
+
+instance NFData A.Token where
+  rnf = rnf . A.prToken
 
 instance (NFData v,NFData a,Measured v a) => NFData (FingerTree v a) where
   rnf tree = foldl1 (seq . rnf) tree `seq` rnf (measure tree)
@@ -39,46 +42,35 @@ instance NFData Suffix where
   rnf (Multi toks) = rnf toks
 
 allTest :: [String]
-allTest = ["Alex","Update.Measure","Update","Update.Tree"]
+allTest = ["Alex","Update"]
+
+testSizes :: [Int]
+testSizes = 10:[100,200..1900]
 
 main = do
   args <- getArgs
-  let (n,inFiles,tests',arg) = sortArgs args
-  (files,codes) <- case inFiles of
-    [] -> return (["internal"],[core002])
-    _  -> do lazyCode <- mapM (\f -> openFile f ReadMode >>= hGetContents) inFiles
-             return (inFiles,force lazyCode)
+  let (n,inFile,tests',arg) = sortArgs args
+  (file,code) <- case inFile of
+    [] -> return ("internal",core002)
+    _  -> do lazyCode <- openFile inFile ReadMode >>= hGetContents
+             return (inFile,lazyCode)
   let tests = case tests' of
         [] -> allTest
         _  -> tests'
-      codes' = map (concat . P.take n . repeat) codes  -- multiply the size of each file by 20
-      trees' = map J.lexCode codes' -- lexing the input using the bottom-up method
+      codes = map (\i -> (concat . P.take i $ repeat code,show i)) testSizes
       trees = do
-        code <- codes'
+        (code,name) <- codes
         let tree = J.lexCode code
             halfSize = size tree `div` 2
             (left,right) = splitTreeAt halfSize tree
-        return (left, right)
-      measures = do
-        code <- codes'
-        let tree = J.lexCode code
-            halfSize = size tree `div` 2
-            (left,right) = splitTreeAt halfSize tree
-        return (measure left, measure right)
-      sizeTrees = map (\tree -> foldl (helperIncBuilder tree) [] [1..10]) trees'
+        return ((left, right),name)
       testFuns =
-        [ ("Alex",map (benchStuff (show . alexScanTokens)) (zip codes' files))
-        , ("IncLex",map (benchStuff (J.lexCode)) (zip codes' files))
-        , ("Update.Measure",map (benchStuff (getOutState . uncurry mappend))
-                            (zip measures files))
-        , ("Update.Tree",map (benchStuff (getOutState . measure . uncurry mappend))
-                         (zip trees files))
---        , ("Sizes",[bgroup name (map (benchStuff (\tree' -> J.tokens (tree' <> tree'))) (tree))
---                    | (tree,name) <- zip sizeTrees files])
---        , ("AllUp",[bgroup name (map (benchStuff J.tokens) (zip tree (map show [0..])))
---                    | (tree,name) <- zip trees files ])
+        [ ("Alex",map (benchStuff (last . alexScanTokens)) codes)
+        , ("IncLex",map (benchStuff (J.lexCode)) codes)
+        , ("Update",map (benchStuff (getOutState . measure . uncurry mappend)) trees)
         ]
-  trees `deepseq` measures `deepseq` withArgs (tests ++ arg) $
+--      testBig = map (\i -> bgroup (show i) [bgroup tests | (name,tests) <- testFuns])
+  withArgs (tests ++ arg) $
     defaultMain [ bgroup name tests | (name,tests) <- testFuns ]
 
 repeatTree :: Int -> LexTree -> LexTree
@@ -89,20 +81,20 @@ repeatTree i tree = tree <> repeatTree (i-1) tree
 helperIncBuilder :: LexTree -> [(LexTree,String)] -> Int -> [(LexTree,String)]
 helperIncBuilder tree sizeTrees i = sizeTrees ++ [(repeatTree i tree, show i)]
 
-benchStuff :: NFData b => (a -> b) -> (a,String) -> Benchmark
-benchStuff f (x,name) = bench name $ nf f x
+benchStuff :: (NFData b, NFData a) => (a -> b) -> (a,String) -> Benchmark
+benchStuff f (x,name) = x `deepseq` bench name $ nf f x
 
-sortArgs :: [String] -> (Int,[String],[String],[String])
+sortArgs :: [String] -> (Int,String,[String],[String])
 sortArgs args = foldl sortArg (10,[],[],[]) args
   where sortArg (num,files,tests,args) arg@('-':_) =
           (num,files,tests,(arg):args)
         sortArg (num,files,tests,args) ('+':test)  =
-          (num,files,if test `elem` "Sizes":"AllUp":"IncLex":allTest
+          (num,files,if test `elem` "IncLex":allTest
                      then test:tests
                      else tests
                                ,args)
         sortArg (num,files,tests,args) arg | and $ map isDigit arg = (read arg,files,tests,args)
-                                           | otherwise = (num,arg:files,tests,args)
+                                           | otherwise = (num,arg,tests,args)
 
 getOutState :: (Table State Tokens,Size) -> Bool
 getOutState (table,_) = case access table 0 of
