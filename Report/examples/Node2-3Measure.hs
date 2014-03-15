8b602109f37d7086f62c7669f70717ec40eb5426
Jonas-Hugo <jonas.hugo.86@gmail.com>
2014-03-15 14:01:34 +0100

    Added text about measurment for fingertrees

---
+++ Report/examples/Node2-3Measure.hs
@@ -0,0 +1,11 @@
+data Node v a = Node2 v a a | Node3 v a a a
+
+node2 :: (Measured a v) => a -> a -> Node v a
+node2 a b = Node2 ((measure a) $\bullet$ (measure b)) a b
+
+node3 :: (Measured a v) => a -> a -> a -> Node v a
+node3 a b c = Node3 ((measure a) $\bullet$ (measure b) $\bullet$ (measure c)) a b c
+
+instance (Monoid v) => Measured (Node v a) v where
+  measure (Node2 v _ _) = v
+  measure (Node3 v _ _ _) = v
