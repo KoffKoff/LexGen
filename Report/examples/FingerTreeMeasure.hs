450a75fb0481cffbf5ceb31932d1365e7a0441dc
Jonas-Hugo <jonas.hugo.86@gmail.com>
2014-03-15 14:12:21 +0100

    mempty changed to \emptyset

--- Report/examples/FingerTreeMeasure.hs
+++ Report/examples/FingerTreeMeasure.hs
@@ -7,6 +7,6 @@ deep :: (Measured a v) =>
 deep pr m sf = Deep ((measure pr) $\bullet$ (measure m) $\bullet$ (measure sf)) pr m sf
 
 instance (Measured a v) => Measured (FingerTree v a) v where
-  measure Empty      = mempty
+  measure Empty      = $\emptyset$
   measure (Single x) = measure x
   measure (Deep v)   = v
