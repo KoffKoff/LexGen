50cdd415b2a3cd66b75f7a2cbc4c147dcc81ac47
KoffKoff <koffkoffh@gmail.com>
2014-03-25 20:52:10 +0100

    updated used structures and added figures

--- Report/examples/TableMonoid.hs
+++ Report/examples/TableMonoid.hs
@@ -1,9 +1,9 @@
-tabulate :: (State,State) -> (State -> b) -> Table State b
-access :: Table State b -> (State -> b)
+tabulate :: (State,State) -> Transition -> Table
+access :: Table -> Transition
 
 tabulate range f = listArray range [f i | i <- [fst range..snd range]]
 access a x = a ! x
 
-instance Monoid (Table State Tokens) where
+instance Monoid Table where
   mempty = tabulate stateRange (\_ -> emptyTokens)
   f `mappend` g = tabulate stateRange $ combineTokens (access f) (access g)
