ba7f56e3b01d49fe7bc764fd3dc63232b8d6edb2
KoffKoff <koffkoffh@gmail.com>
2014-05-07 05:33:30 +0200

    tweaked memtest and wrote some notes in result cahpter

--- src/Test/MemTest.hs
+++ src/Test/MemTest.hs
@@ -3,21 +3,26 @@ module Test.MemTest where
 import System.Environment
 import System.IO (readFile)
 import Data.ByteString.Lazy (writeFile)
-import Test.Java hiding (encode)
+import Test.Java as J hiding (encode)
 import Test.Lexjava
 import Data.FingerTree
 import Data.Monoid
 import Data.Binary as Bin
 import Prelude hiding (writeFile)
+import Control.Monad
 
 main = do
   args <- getArgs
   print "usage: MemTets <iterations> <code file> <output file(s)>"
   memTest (read $ head args) (args !! 1) (args !! 2)
 
-memTest :: Int -> String -> String -> IO ()
-memTest iter inFile outFile = do
+memReader :: Int -> String -> String -> IO ()
+memReader i inFile outFile = do
   code <- readFile inFile
+  memTest i code outFile
+
+memTest :: Int -> String -> String -> IO ()
+memTest iter code outFile = do
   fileWriter iter (outFile ++ ".inclex") (makeTree code) measure
 
 fileWriter :: (Binary b,Monoid a) => Int -> String -> a -> (a -> b) -> IO ()
@@ -26,6 +31,7 @@ fileWriter i file code f | i <= 0 = return ()
   writeFile (file ++ "." ++ show i) $ (encode . f) code
   fileWriter (i-1) file (code <> code) f
 
+-- Serialization for the Tokens data structure
 instance Binary Tokens where
   put NoTokens = put (0 :: Word8)
   put (InvalidTokens seq) = do put (1 :: Word8)
@@ -35,7 +41,11 @@ instance Binary Tokens where
                                  put suff
                                  put int
 
-  get = return NoTokens
+  get = do tag <- getWord8
+           case tag of
+             0 -> return NoTokens
+             1 -> liftM InvalidTokens get
+             2 -> liftM3 Tokens get get get
 
 instance Binary Suffix where
   put (Str seq) = do put (0 :: Word8)
@@ -45,14 +55,36 @@ instance Binary Suffix where
   put (Multi toks) = do put (2 :: Word8)
                         put toks
 
-  get = return (Str mempty)
+  get = do tag <- getWord8
+           case tag of
+             0 -> liftM Str get
+             1 -> liftM One get
+             2 -> liftM Multi get
 
 instance Binary IntToken where
-  put (Token seq accs) = put seq
+  put (Token seq accs) = do put seq
+                            put accs
 
-  get = return (Token mempty [])
+  get = liftM2 Token get get
+
+instance Binary (J.AlexAcc a b) where
+  put (J.AlexAcc a) = put (0 :: Word8)
+  put J.AlexAccSkip = put (1 :: Word8)
+  
+  get = do tag <- getWord8
+           case tag of
+             0 -> return J.AlexAccSkip
+             1 -> return J.AlexAccSkip
 
 instance Binary a => Binary (Sum a) where
   put (Sum a) = put a
 
   get = get
+
+core002 :: String
+core002 = "/*@ @@*/\n\n" ++
+          "// hej\n" ++
+          "int foo.thur() {\n" ++
+          "printString(\"foo\");\n" ++
+          "return if true then else if 1 else null;\n" ++
+          "}"
