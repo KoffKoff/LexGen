424fc3c27312b0877d8b9b216fde7e44d8fd9373
KoffKoff <koffkoffh@gmail.com>
2014-07-14 14:04:19 +0200

    Worked on space complexity

--- Test.hs
+++ Test.hs
@@ -3,16 +3,19 @@ module Test where
 import Text.Printf
 import System.IO
 
-f :: Integer -> Integer
-f n = s1 0
-  where s1 x | s2 x 0 < n  = n - s2 x 0 + s1 (x+1)
-             | otherwise = 0
-        s2 x y | y < x = 2^y + s2 x (y+1)
-        s2 _ _ | True  = 0
+--f :: Integer -> Integer
+--f n = sum sum1
+--  where sum1 = [n - s2 | s2 <- takeWhile (<= n) pow]
+
+check2 x = sum (map (flip (-) 1) (take x pow)) == 2^(x) - x - 1
+
+check x = sum (take x pow) == 2^x -1
+
+pow = [2^y | y <- [0..]]
 
 lol :: [String] -> IO ()
 lol xs = writeFile "data.csv" $ printf "%-25s%s" "n" "f(n)" ++ "\n" ++ unlines xs
 
-pps :: [String]
-pps = map pp [10^x | x <- [0..]]
-  where pp n = printf "%-25d%d" n (f n)
+--pps :: [String]
+--pps = map pp [10^x | x <- [0..]]
+--  where pp n = printf "%-25d%d" n (f n)
