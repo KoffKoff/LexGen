032a4cc445d3af31427adcdec6ff52726cb3c482
KoffKoff <koffkoffh@gmail.com>
2014-08-12 00:11:26 +0200

    minor tweaking and spell checking

--- Report/examples/MergeSuff.hs
+++ Report/examples/MergeSuff.hs
@@ -1,7 +1,7 @@
 mergeSuff :: Suffix -> Suffix -> Transition -> Suffix
 mergeSuff (Multi toks1) suff2 trans2 = Multi $
   let newToks = combineWithRHS toks1 trans2
-  in if isValid $ newToks
+  in if isValid newToks
      then newToks
      else let newSuff = mergeSuff (lastToken toks1) suff2 trans2
           in toks1 {lastToken = newSuff}
